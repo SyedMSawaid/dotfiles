5c5502510439eef8abb9d151ba81df488874df88
Syed M. Sawaid <34231494+SyedMSawaid@users.noreply.github.com>
2026-05-20 00:16:53 +0200

    updated convert-mp4-to-mov script

--- scripts/convert-mp4-to-mov-1080.sh
+++ scripts/convert-mp4-to-mov-1080.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+mkdir -p converted
 for f in *.mp4; do
   ffmpeg -hwaccel cuda -hwaccel_output_format cuda \
     -i "$f" \
     -vf "scale_cuda=1920:1080,hwdownload,format=nv12" \
     -c:v hevc_nvenc -rc vbr -cq 20 -preset p6 \
     -c:a copy \
-    "${f%.mp4}_1080p.mov"
+    "converted/${f%.mp4}_1080p.mov"
 done
